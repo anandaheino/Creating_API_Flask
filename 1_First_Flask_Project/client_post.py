10d8ed6271d89a32723a3485a520f8858ae2895f
AnandaHHC <anandaheino2@gmail.com>
2021-09-08 17:41:13 -0300

    Using the method POST from requests.

---
+++ 1_First_Flask_Project/client_post.py
@@ -0,0 +1,11 @@
+import requests
+
+# USING THE METHOD POST FROM REQUESTS:
+data = {'info': 'pay', 'value': 5000}
+response = requests.post("http://127.0.0.1:5000/informations", data=data)
+
+# Analysing the status code:
+if response.status_code == 200:
+    print(response.json())
+else:
+    print("Sorry, something wen't wrong. Status code:",(response.status_code))
