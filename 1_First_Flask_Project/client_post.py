03a4891e0ca4555f1343f4894a12fd27942015d1
AnandaHHC <anandaheino2@gmail.com>
2021-09-12 16:55:48 -0300

    Adding a new employee to the database using POST method.

--- 1_First_Flask_Project/client_post.py
+++ 1_First_Flask_Project/client_post.py
@@ -1,8 +1,12 @@
 import requests
 
-# USING THE METHOD POST FROM REQUESTS:
-data = {'username':'Math', 'secret':'@admin2', 'info': 'pay', 'value': 4000}
-response = requests.post("http://127.0.0.1:5000/informations", data=data)
+# USING THE METHOD POST FROM REQUESTS TO RECEIVE INFORMATIONS:
+#data = {'username':'Ananda', 'secret':'@admin1', 'info': 'pay', 'value': 4000}
+#response = requests.post("http://127.0.0.1:5000/informations", data=data)
+
+# REGISTER NEW EMPLOYEES:
+data = {'username':'Ananda', 'secret':'@admin1', 'name': 'Ananda', 'position': 'DataScientist', 'pay': 5000}
+response = requests.post("http://127.0.0.1:5000/register", data=data)
 
 # Analysing the status code:
 if response.status_code == 200:
